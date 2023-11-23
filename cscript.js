4bdd1254b40438fa44e1e5a0e4a42877fcc1f15c
g1tismyHub <140825245+g1tismyHub@users.noreply.github.com>
2023-11-23 21:57:23 +0800

    Add files via upload

---
+++ cscript.js
@@ -0,0 +1,14 @@
+result = 0;
+
+function sub(){
+   result--;
+   document.getElementById("num").innerHTML = result;
+}
+function rstart(){
+   result=0;
+   document.getElementById("num").innerHTML = result;
+}
+function addbuton(){
+   result++;
+   document.getElementById("num").innerHTML = result;
+}
